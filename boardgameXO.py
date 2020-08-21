1adc96eec6d2832ed367d4a3a3a57f3058e6e5c2
Thanakrit <s6201012620091@email.kmutnb.ac.th>
2020-08-21 17:03:28 +0700

    Fix method retrieveValue() in class Board

--- boardgameXO.py
+++ boardgameXO.py
@@ -1,25 +1,25 @@
 class Board:
     def __init__(self):
         self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," "]]
-        self.getMark = {'1': 'X', '2': 'O'}
-        self.player = 2
+        self.getMark = { 1 : "X", 2 : "O" }
+        self.player = 1
         self.index = ""
 
     def startGame(self):
         self.round = 1
         print("Start Game : Tic-Tac-Toe!")
-
+        
         while (self.round < 10):
-            if self.player == "" or self.player == 2:
-                mark = self.getMark[str(self.player)]
+            if self.player == "" or self.player == 1:
+                mark = self.getMark[self.player]
                 pos = int(input("Player 1 turn \nEnter your position 'X': "))
             
             else :
-                self.player == 1
-                mark = self.getMark[str(self.player)]
+                self.player == 2
+                mark = self.getMark[self.player]
                 pos = int(input("Player 2 turn \nEnter your position 'O': "))
 
-            class3.placeMarker(mark, pos)
+            class3.placeMarker(self.player, pos)
             class2.drawBoard()
 
             if self.winCheck(self.player) == False:
@@ -37,27 +37,27 @@ class Board:
         else:
             print("No player win :( \n!!!Game Over!!!")
             
-    def setBoard(self, marker, position): ## ได้ None 
+    def setBoard(self, player, position): ## ได้ None 
         if position <= 3 and self.listBoard[0][position-1] == " ":
-            self.listBoard[0][position-1] = marker
+            self.listBoard[0][position-1] = self.getMark[player]
                 
         elif 3 < position <= 6 and self.listBoard[1][position-4] == " ":
-            self.listBoard[1][position-4] = marker
+            self.listBoard[1][position-4] = self.getMark[player]
         
         elif 6 < position <= 9 and self.listBoard[2][position-7] == " ":
-            self.listBoard[2][position-7] = marker
+            self.listBoard[2][position-7] = self.getMark[player]
 
         else:
             return False
 
     def retrieveValue(self, position): ## get value form listBoard
-        if position <= 3 and self.listBoard[0][position-1] == " ":
+        if position <= 3 :
             return self.listBoard[0][position-1]
                 
-        elif 3 < position <= 6 and self.listBoard[1][position-4] == " ":
+        elif 3 < position <= 6 :
             return self.listBoard[1][position-4]
         
-        elif 6 < position <= 9 and self.listBoard[2][position-7] == " ":
+        elif 6 < position <= 9 :
             return self.listBoard[2][position-7]
         
     def winCheck(self, player): ## check winner
@@ -82,35 +82,14 @@ class Printer:
         for i in range(1,10,3):
             print(class1.retrieveValue(i), " | ", class1.retrieveValue(i+1), " | ", class1.retrieveValue(i+2))
     
-class TextInput: 
-    def placeMarker(self, marker, position): ## place the marker in list of board
-        class1.setBoard(marker, position)
-        
-        """
-        if class1.setBoard(position, player) == False:
-            print("This position has marker! Please enter other position")
-            pos = int(input("Enter your new position : "))
-            self.placeMarker(player, pos)
-        else:
-            pass
-        """
-    
-    """
+class TextInput:
     def placeMarker(self, player, position): ## place the marker in list of board
-        if position <= 3 and class1.listBoard[0][position-1] == " ":
-            self.listBoard[0][position-1] = player
-                
-        elif 3 < position <= 6 and class1.listBoard[1][position-4] == " ":
-            self.listBoard[1][position-4] = player
-        
-        elif 6 < position <= 9 and class1.listBoard[2][position-7] == " ":
-            self.listBoard[2][position-7] = player
-
-        else:
+        if class1.setBoard(player, position) == False:
             print("This position has marker! Please enter other position")
             pos = int(input("Enter your new position : "))
             self.placeMarker(player, pos)
-    """
+        
+
 
 class1 = Board()
 class2 = Printer()
